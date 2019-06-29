a6e2508a8ae7fc567561abc7eafa4379e22fe3d9
primys <a2uso@tuta.io>
2019-06-28 19:21:38 -0700

    Add struct for Transaction

--- BlockExplorer.sol
+++ BlockExplorer.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.4.18;
 
 contract BlockExplorer {
-    function BlockExplorer(){
-
+    struct Transaction {
+        uint256 timestamp;
+        address from;
+        address to;
+        uint256 value;
+        string hash;
     }
+
+    function BlockExplorer() { }
 }
