5e961d4c4d207629bb343da28d90ccb4c7f21ffd
primys <a2uso@tuta.io>
2019-06-28 20:58:14 -0700

    Add Purchase struct

--- Purchases.sol
+++ Purchases.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.0;
 
 contract Purchases {
-    function Purchases(){
-
+    struct Purchase {
+        string appId;
+        string purchaseId;
+        uint256 cost;
+        uint256 timestamp;
     }
+
 }
