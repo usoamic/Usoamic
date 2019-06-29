5144342bce6b54470bac306cc2a67657f5bc3b9b
primys <a2uso@tuta.io>
2019-06-28 19:59:29 -0700

    Add function for getting and adding transaction

--- BlockExplorer.sol
+++ BlockExplorer.sol
@@ -1,16 +1,44 @@
 pragma solidity ^0.4.18;
 
+import "./library\AddressUtil.sol";
+
 contract BlockExplorer {
+    using AddressUtil for address;
+
     struct Transaction {
-        uint256 timestamp;
         address from;
         address to;
         uint256 value;
-        string hash;
+        uint256 timestamp;
     }
 
     mapping(address => Transaction[]) private addressTransactions;
     Transaction[] private transactions;
 
     function BlockExplorer() { }
+
+    function addTransaction(address to, uint256 value) private {
+        require(!to.isEmpty && value > 0);
+
+        Transaction tx = Transaction({
+            from: msg.sender,
+            to: to,
+            value: value,
+            timestamp: now
+        });
+
+        addressTransactions[to].push(tx);
+        addressTransactions[msg.sender].push(tx);
+        transactions.push(tx);
+    }
+
+    function getTransaction(address addr, uint256 id) public view returns(address from, address to, uint256 value, uint256 timestamp) {
+        Transaction storage tx = addressTransactions[addr][id];
+        return (tx.from, tx.to, tx.value, tx.timestamp);
+    }
+
+    function getTransaction(uint256 id) public view returns(address from, address to, uint256 value, uint256 timestamp) {
+        Transaction storage tx = transactions[id];
+        return (tx.from, tx.to, tx.value, tx.timestamp);
+    }
 }
