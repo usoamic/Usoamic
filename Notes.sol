226524c555d6ed655a6a81d4ab17ba0b39d0767b
primys <a2uso@tuta.io>
2019-06-29 04:46:42 -0700

    Add methods for getting notes

--- Notes.sol
+++ Notes.sol
@@ -48,4 +48,30 @@ contract Notes {
             notes.push(noteRef);
         }
     }
+
+    function getNoteByAddress(address author, uint256 noteId) view public returns(bool exist, uint256 idOfNote, NoteVisibility visibility, int256 refId, string content, address author, uint256 timestamp) {
+        if (!isExistNoteByAuthor(author, noteId)) {
+            (exist, idOfNote) = (false, noteId);
+            return;
+        }
+        Note storage note = addressNotes[author][noteId];
+        return (true, noteId, note.visibility, note.refId, note.content, note.author, note.timestamp);
+    }
+
+    function getNote(uint256 noteId) view public returns(bool exist, uint256 idOfNote, NoteVisibility visibility, int256 refId, string content, address author, uint256 timestamp) {
+        if(!isExistNoteRef(noteId)) {
+            (exist, idOfNote) = (false, noteId);
+            return;
+        }
+        NoteRef storage noteRef = notes[noteId];
+        return getNoteByAddress(noteRef.author, noteRef.noteId);
+    }
+
+    function isExistNoteByAuthor(address author, uint256 noteId) view private returns(bool) {
+        return ((noteId < addressNotes[author].length) && (noteId >= 0));
+    }
+
+    function isExistNoteRef(uint256 noteId) view private returns(bool) {
+        return ((noteId < notes.length) && (noteId >= 0));
+    }
 }
