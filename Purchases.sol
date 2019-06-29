50e4667a56ef6c97cd769d5122a6c2f061a488cc
primys <a2uso@tuta.io>
2019-06-28 20:59:42 -0700

    Add code to make purchase

--- Purchases.sol
+++ Purchases.sol
@@ -8,4 +8,16 @@ contract Purchases {
         uint256 timestamp;
     }
 
+    mapping(address => Purchase[]) private purchases;
+
+    function makePurchase(string _appId, string _purchaseId, uint256 _cost) {
+        transfer(owner, _cost);
+        Purchase purchase = Purchase({
+            appId: _appId,
+            purchaseId: _purchaseId,
+            cost: _cost,
+            timestamp: now
+            });
+        purchases[msg.sender].push(purchase);
+    }
 }
