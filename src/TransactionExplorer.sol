2315bd51ab3dc8f7045a3096e6f51395247c5ebb
primys <a2uso@tuta.io>
2019-06-29 06:19:44 -0700

    Code refactoring: remove TransactionRef, add addTransactionToAddress method and etc

--- src/TransactionExplorer.sol
+++ src/TransactionExplorer.sol
@@ -6,49 +6,48 @@ contract TransactionExplorer is Purchases {
     using AddressUtil for address;
 
     struct Transaction {
+        uint256 txId;
         address from;
         address to;
         uint256 value;
         uint256 timestamp;
     }
 
-    struct TransactionRef {
-        uint256 txId;
-        address sender;
-    }
-
     mapping(address => uint256) private numberOfAddressTransactions;
-    mapping(address => mapping(uint256 => Transaction)) private addressTransactions;
-    mapping(uint256 => TransactionRef) private transactions;
+    mapping(address => mapping(uint256 => uint256)) private addressTransactions;
+    mapping(uint256 => Transaction) private transactions;
 
     uint256 private numberOfTransactions = 0;
 
     function addTransaction(address _to, uint256 _value) onlyUnfrozen internal {
         require(_value > 0);
 
-        addressTransactions[_to][numberOfAddressTransactions[msg.sender]] = Transaction({
+        putTransaction(msg.sender);
+        putTransaction(_to);
+
+        transactions[numberOfTransactions] = Transaction({
+            txId: numberOfTransactions,
             from: msg.sender,
             to: _to,
             value: _value,
             timestamp: now
         });
 
-        transactions[numberOfTransactions] = TransactionRef({
-            txId: msg.sender,
-            sender: msg.sender
-        });
-
-        numberOfAddressTransactions[msg.sender]++;
         numberOfTransactions++;
     }
 
-    function getTransactionByAddress(address _owner, uint256 _txId) public view returns(bool exist, uint256 txId, address from, address to, uint256 value, uint256 timestamp) {
-        if(!isExistTransactionBySender(_owner, _txId)) {
-            (exist, txId) = (false, _txId);
+    function addTransactionToAddress(address _owner, uint256 txId) private {
+        addressTransactions[msg.sender][numberOfAddressTransactions[msg.sender]] = numberOfTransactions;
+        numberOfAddressTransactions[_owner]++;
+    }
+
+    function getTransactionByAddress(address _owner, uint256 _txNumber) public view returns(bool exist, uint256 txId, uint256 txNumber, address from, address to, uint256 value, uint256 timestamp) {
+        if(!isExistTransactionBySender(_owner, _txNumber)) {
+            (exist, txId) = (false, _txNumber);
             return;
         }
-        Transaction storage tx = addressTransactions[_owner][_txId];
-        return (true, _txId, tx.from, tx.to, tx.value, tx.timestamp);
+        Transaction storage tx = addressTransactions[_owner][_txNumber];
+        return (true, txId_txNumber, tx.from, tx.to, tx.value, tx.timestamp);
     }
 
     function getTransaction(uint256 _txId) public view returns(bool exist, uint256 txId, address from, address to, uint256 value, uint256 timestamp) {
@@ -56,8 +55,8 @@ contract TransactionExplorer is Purchases {
             (exist, txId) = (false, _txId);
             return;
         }
-        Transaction storage tx = transactions[_txId];
-        return (true, _txId, tx.from, tx.to, tx.value, tx.timestamp);
+        TransactionRef storage tx = transactions[_txId];
+        return getTransactionByAddress(tx.from, _txId);
     }
 
     function numberOfTransactions() public {
