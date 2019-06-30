532594fad1117b5ee852196b0c91bea5d2ccaed2
primys <a2uso@tuta.io>
2019-06-30 00:35:06 -0700

    Add methods for ethereum deposit and swap

--- src/Swap.sol
+++ src/Swap.sol
@@ -3,5 +3,15 @@ pragma solidity ^0.4.18;
 import "./TransactionExplorer.sol";
 
 contract Swap is TransactionExplorer {
+    uint256 private swapDeposit = 0;
 
+    function depositEth() onlyOwner payable {
+        swapDeposit += msg.value;
+    }
+
+    function burnSwap(uint256 _value) onlyUnfrozen public {
+        uint256 numberOfWei = _value*1;
+        msg.sender.transfer(_value);
+        burn(_value);
+    }
 }
