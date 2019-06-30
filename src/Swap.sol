261dabacd89c5d8ed934114511872806ddb4e80e
primys <a2uso@tuta.io>
2019-06-30 00:36:50 -0700

    Add swapRate

--- src/Swap.sol
+++ src/Swap.sol
@@ -4,13 +4,18 @@ import "./TransactionExplorer.sol";
 
 contract Swap is TransactionExplorer {
     uint256 private swapDeposit = 0;
+    uint256 private swapRate = 1;
 
     function depositEth() onlyOwner payable {
         swapDeposit += msg.value;
     }
 
+    function setSwapRate(uint256 _value) onlyOwner public {
+        swapRate = _value;
+    }
+
     function burnSwap(uint256 _value) onlyUnfrozen public {
-        uint256 numberOfWei = _value*1;
+        uint256 numberOfWei = _value*swapRate;
         msg.sender.transfer(_value);
         burn(_value);
     }
