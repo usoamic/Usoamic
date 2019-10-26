8c79ef6066aa736f1da51748596b274acc32047d
primys <a2uso@tuta.io>
2019-10-26 07:57:23 -0700

    Update version

--- src/Usoamic.sol
+++ src/Usoamic.sol
@@ -11,7 +11,7 @@ contract Usoamic is Swap {
 
     string public name = "Usoamic";
     string public symbol = "USO";
-    string public version = "v2";
+    string public version = "v2.1";
     uint256 public coinSupply = 25000000;
     uint256 public decimals = 8;
 
